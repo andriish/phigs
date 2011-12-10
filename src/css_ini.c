336269dba660b4f69f5a670aadcff5d617f05314
alexuspol <alexuspol@e18fabcd-5fc6-4edd-8a17-43b1c1a820ff>
2011-12-10 11:37:28 +0000

    Added copyright header for all files

--- src/css_ini.c
+++ src/css_ini.c
@@ -1,6 +1,22 @@
-/* $XConsortium: css_ini.c,v 5.2 94/04/17 20:42:40 rws Exp $ */
-
-/***********************************************************
+/******************************************************************************
+*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+*
+*   This file is part of Open PHIGS
+*   Copyright (C) 2011 Surplus Users Ham Society
+*
+*   Open PHIGS is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Lesser General Public License as published by
+*   the Free Software Foundation, either version 2.1 of the License, or
+*   (at your option) any later version.
+*
+*   Open PHIGS is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Lesser General Public License for more details.
+*
+*   You should have received a copy of the GNU Lesser General Public License
+*   along with Open PHIGS. If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************
 
 Copyright (c) 1989,1990, 1991  X Consortium
 
