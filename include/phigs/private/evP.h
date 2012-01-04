84349a2c0f3ef7912fb5f212ae55f0940e1c6b88
alexuspol <alexuspol@e18fabcd-5fc6-4edd-8a17-43b1c1a820ff>
2012-01-04 18:40:40 +0000

    Fixed mistakes

--- include/phigs/private/evP.h
+++ include/phigs/private/evP.h
@@ -1,3 +1,23 @@
+/******************************************************************************
+*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+*
+*   This file is part of Open PHIGS
+*   Copyright (C) 2011 - 2012 Surplus Users Ham Society
+*
+*   Open PHIGS is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Lesser General Public License as published by
+*   the Free Software Foundation, either version 2.1 of the License, or
+*   (at your option) any later version.
+*
+*   Open PHIGS is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Lesser General Public License for more details.
+*
+*   You should have received a copy of the GNU Lesser General Public License
+*   along with Open PHIGS. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #ifndef _evP_h
 #define _evP_h
 
@@ -23,6 +43,108 @@ typedef struct {
    Pint num_events;
    List *events;
 } Phg_ev_tbl;
+
+/*******************************************************************************
+ * phg_ev_tbl_create
+ * 
+ * DESCR:       Create event table
+ * RETURNS:     Pointer to event table or NULL
+ */
+
+Phg_ev_tbl* phg_ev_tbl_create(
+   int num_events
+   );
+
+/*******************************************************************************
+ * phg_ev_tbl_init
+ *
+ * DESCR:       Inititalize event table
+ * RETURNS:     TRUE or FALSE
+ */
+
+int phg_ev_tbl_init(
+   Phg_ev_tbl *ev_tbl
+   );
+
+/*******************************************************************************
+ * phg_ev_tbl_destroy
+ *
+ * DESCR:       Destroy event table
+ * RETURNS:     N/A
+ */
+
+void phg_ev_tbl_destroy(
+   Phg_ev_tbl *ev_tbl
+   );
+
+/*******************************************************************************
+ * phg_ev_register
+ *
+ * DESCR:       Register an event in event table
+ * RETURNS:     TRUE or FALSE
+ */
+
+int phg_ev_register(
+   Phg_ev_tbl *ev_tbl,
+   Display *display,
+   Window window,
+   int event_type,
+   caddr_t cdata,
+   void (*callback)(Display*, Window, caddr_t, XEvent*)
+   );
+
+/*******************************************************************************
+ * phg_ev_unregister
+ * 
+ * DESCR:       Unregister an event in event table
+ * RETURNS:     N/A
+ */
+
+void phg_ev_unregister(
+   Phg_ev_tbl *ev_tbl,
+   Display *display,
+   Window window,
+   int event_type,
+   caddr_t cdata
+   );
+
+/*******************************************************************************
+ * phg_ev_unregister_display
+ *
+ * DESCR:       Unregister all events for display in event table
+ * RETURNS:     N/A
+ */
+
+void phg_ev_unregister_display(
+   Phg_ev_tbl *ev_tbl,
+   Display *display
+   );
+
+/*******************************************************************************
+ * phg_ev_unregister_window
+ *
+ * DESCR:       Unregister all events for window in event table
+ * RETURNS:     N/A
+ */
+
+void phg_ev_unregister_window(
+   Phg_ev_tbl *ev_tbl,
+   Display *display,
+   Window window
+   );
+
+/*******************************************************************************
+ * phg_ev_dispatch
+ * 
+ * DESCR:       Dipatch events using event table
+ * RETURNS:     N/A
+ */
+
+void phg_ev_dispatch(
+   Phg_ev_tbl *ev_tbl,
+   Display *display,
+   XEvent *event
+   );
 
 #endif /* _evP_h */
 
--- src/libphigs/inp_ev.c
+++ src/libphigs/inp_ev.c
@@ -1,7 +1,34 @@
+/******************************************************************************
+*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+*
+*   This file is part of Open PHIGS
+*   Copyright (C) 2011 - 2012 Surplus Users Ham Society
+*
+*   Open PHIGS is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Lesser General Public License as published by
+*   the Free Software Foundation, either version 2.1 of the License, or
+*   (at your option) any later version.
+*
+*   Open PHIGS is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Lesser General Public License for more details.
+*
+*   You should have received a copy of the GNU Lesser General Public License
+*   along with Open PHIGS. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <phigs/phg.h>
 #include <phigs/private/evP.h>
+
+/*******************************************************************************
+ * phg_ev_tbl_create
+ *
+ * DESCR:       Create event table
+ * RETURNS:     Pointer to event table or NULL
+ */
 
 Phg_ev_tbl* phg_ev_tbl_create(
    int num_events
@@ -11,11 +38,10 @@ Phg_ev_tbl* phg_ev_tbl_create(
 
    ev_tbl = (Phg_ev_tbl *) malloc(sizeof(Phg_ev_tbl) +
                                   sizeof(Phg_ev_entry) * num_events);
-
    if (ev_tbl != NULL) {
       ev_tbl->num_events = num_events;
-      ev_tbl->events = (Phg_ev_entry *) &ev_tbl[1];
-      if (!phg_ev_init(ev_tbl)) {
+      ev_tbl->events = (List *) &ev_tbl[1];
+      if (!phg_ev_tbl_init(ev_tbl)) {
          free(ev_tbl);
          ev_tbl = NULL;
       }
@@ -23,6 +49,13 @@ Phg_ev_tbl* phg_ev_tbl_create(
 
    return ev_tbl;
 }
+
+/*******************************************************************************
+ * phg_ev_tbl_init
+ *
+ * DESCR:       Inititalize event table
+ * RETURNS:     TRUE or FALSE
+ */
 
 int phg_ev_tbl_init(
    Phg_ev_tbl *ev_tbl
@@ -37,12 +70,26 @@ int phg_ev_tbl_init(
    return TRUE;
 }
 
+/*******************************************************************************
+ * phg_ev_tbl_destroy
+ *
+ * DESCR:       Destroy event table
+ * RETURNS:     N/A
+ */
+
 void phg_ev_tbl_destroy(
    Phg_ev_tbl *ev_tbl
    )
 {
    free(ev_tbl);
 }
+
+/*******************************************************************************
+ * phg_ev_register
+ *
+ * DESCR:       Register an event in event table
+ * RETURNS:     TRUE or FALSE
+ */
 
 int phg_ev_register(
    Phg_ev_tbl *ev_tbl,
@@ -85,6 +132,13 @@ int phg_ev_register(
    return status;
 }
 
+/*******************************************************************************
+ * phg_ev_unregister
+ *
+ * DESCR:       Unregister event in event table
+ * RETURNS:     N/A
+ */
+
 void phg_ev_unregister(
    Phg_ev_tbl *ev_tbl,
    Display *display,
@@ -109,47 +163,68 @@ void phg_ev_unregister(
    }
 }
 
+/*******************************************************************************
+ * phg_ev_unregister_display
+ *
+ * DESCR:       Unregister all events for display in event table
+ * RETURNS:     N/A
+ */
+
 void phg_ev_unregister_display(
    Phg_ev_tbl *ev_tbl,
-   Display *display,
-   int event_type
+   Display *display
    )
 {
+   int i;
    Phg_ev_entry *ev;
 
-   /* First check if entry exists */
-   for (ev = (Phg_ev_entry *) LIST_HEAD(&ev_tbl->events[event_type]);
-        ev != NULL;
-        ev = (Phg_ev_entry *) NODE_NEXT(&ev->node)) {
-      if (ev->display == display) {
-         list_remove(&ev_tbl->events[event_type], &ev->node);
-         free(ev);
-         break;
+   for (i = 0; i < ev_tbl->num_events; i++) {
+      for (ev = (Phg_ev_entry *) LIST_HEAD(&ev_tbl->events[i]);
+           ev != NULL;
+           ev = (Phg_ev_entry *) NODE_NEXT(&ev->node)) {
+         if (ev->display == display) {
+            list_remove(&ev_tbl->events[i], &ev->node);
+            free(ev);
+         }
       }
    }
 }
+
+/*******************************************************************************
+ * phg_ev_unregister_window
+ *
+ * DESCR:       Unregister all events for window in event table
+ * RETURNS:     N/A
+ */
 
 void phg_ev_unregister_window(
    Phg_ev_tbl *ev_tbl,
    Display *display,
-   Window window,
-   int event_type
+   Window window
    )
 {
+   int i;
    Phg_ev_entry *ev;
 
-   /* First check if entry exists */
-   for (ev = (Phg_ev_entry *) LIST_HEAD(&ev_tbl->events[event_type]);
-        ev != NULL;
-        ev = (Phg_ev_entry *) NODE_NEXT(&ev->node)) {
-      if ((ev->display == display) &&
-          (ev->window == window)) {
-         list_remove(&ev_tbl->events[event_type], &ev->node);
-         free(ev);
-         break;
+   for (i = 0; i < ev_tbl->num_events; i++) {
+      for (ev = (Phg_ev_entry *) LIST_HEAD(&ev_tbl->events[i]);
+           ev != NULL;
+           ev = (Phg_ev_entry *) NODE_NEXT(&ev->node)) {
+         if ((ev->display == display) &&
+             (ev->window == window)) {
+            list_remove(&ev_tbl->events[i], &ev->node);
+            free(ev);
+         }
       }
    }
 }
+
+/*******************************************************************************
+ * phg_ev_dispatch
+ *
+ * DESCR:       Dipatch events using event table
+ * RETURNS:     N/A
+ */
 
 void phg_ev_dispatch(
    Phg_ev_tbl *ev_tbl,
