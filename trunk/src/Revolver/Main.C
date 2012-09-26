9cfb632b355431bc66f79446e179fb7fe1d20895
alexuspol <alexuspol@e18fabcd-5fc6-4edd-8a17-43b1c1a820ff>
2012-09-26 14:55:46 +0000

    Moved workstation from Vk to Revolver

--- trunk/src/Revolver/Main.C
+++ trunk/src/Revolver/Main.C
@@ -1,44 +1,76 @@
-/******************************************************************************
-*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
-*
-*   This file is part of Open PHIGS
-*   Copyright (C) 2011 - 2012 Surplus Users Ham Society
-*
-*   Open PHIGS is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU Lesser General Public License as published by
-*   the Free Software Foundation, either version 2.1 of the License, or
-*   (at your option) any later version.
-*
-*   Open PHIGS is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU Lesser General Public License for more details.
-*
-*   You should have received a copy of the GNU Lesser General Public License
-*   along with Open PHIGS. If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
+///////////////////////////////////////////////////////////////////////////////
+//  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+//
+//  This file is part of Open PHIGS
+//  Copyright (C) 2011 - 2012 Surplus Users Ham Society
+//
+//  Open PHIGS is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 2.1 of the License, or
+//  (at your option) any later version.
+//
+//  Open PHIGS is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with Open PHIGS. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
-#include "DrawingArea.H"
+#include <phigs/phg.h>
+#include "Workstation.h"
 
-Widget top_level;
-XtAppContext app_con;
+void initStructs()
+{
+   Ppoint quad_pts[4] = {
+      {0.0, 0.0},
+      {0.5, 0.0},
+      {0.5, 0.5},
+      {0.0, 0.5}
+   };
+   Ppoint_list quad = {4, quad_pts};
+   Pgcolr blue;
+
+   blue.type = PMODEL_RGB;
+   blue.val.general.x = 0.0;
+   blue.val.general.y = 0.0;
+   blue.val.general.z = 1.0;
+
+   popen_struct(0);
+   pset_int_colr(&blue);
+   pfill_area(&quad);
+   pclose_struct();
+}
 
 int main(int argc, char *argv[])
 {
-  top_level = XtVaAppInitialize(
-        &app_con,              /* Application context */
+    Widget toplevel;
+    XtAppContext app;
+
+    popen_phigs(NULL, 0);
+    initStructs();
+
+    toplevel = XtVaAppInitialize(
+        &app,                  /* Application context */
         "Revolver",            /* Application name */
         NULL, 0,               /* Command line options list */
         &argc, argv,           /* Command line args */
         NULL,                  /* Fallback resources */
-        NULL);                 /* Terminate varargs list */
+        NULL);
 
-   DrawingArea *area = new DrawingArea(top_level);
+    Workstation *ws = new Workstation("ws", toplevel, 0);
 
-   XtRealizeWidget(top_level);
-   XtAppMainLoop(app_con);
+    std::cout << "Class name: " << ws->className() << std::endl;
+    std::cout << "Object name: " << ws->name() << std::endl;
+    std::cout << "Workstation id: " << ws->id() << std::endl;
 
-   return 0;
+    ws->show();
+
+    XtRealizeWidget(toplevel);
+    XtAppMainLoop(app);
+
+    return 0;
 }
 
