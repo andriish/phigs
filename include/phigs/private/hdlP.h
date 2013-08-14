2523b371ce4ed0085b72e89595c5318477bf2edd
alexuspol <alexuspol@e18fabcd-5fc6-4edd-8a17-43b1c1a820ff>
2013-08-14 19:29:13 +0000

    Separated handler functions in two files: hdl.c and hdl_plus.c

--- include/phigs/private/hdlP.h
+++ include/phigs/private/hdlP.h
@@ -27,13 +27,49 @@
 #include <phigs/private/cssP.h>
 
 /*******************************************************************************
- * phg_handle_int_list
+ * hdl_create
+ *
+ * DESCR:       Create new element handle
+ * RETURNS:     Pointer to element head or NULL
+ */
+
+Phg_elmt_info* hdl_create(
+   void **data,
+   caddr_t argdata
+   );
+
+/*******************************************************************************
+ * hdl_resize
+ *
+ * DESCR:       Change size element handle
+ * RETURNS:     Pointer to element head or NULL
+ */
+
+Phg_elmt_info* hdl_resize(
+   void *buf,
+   void **data,
+   caddr_t argdata
+   );
+
+/*******************************************************************************
+ * hdl_dup
+ *
+ * DESCR:       Duplicate element handle
+ * RETURNS:     Pointer to element head or NULL
+ */
+
+Phg_elmt_info* hdl_dup(
+   caddr_t argdata
+   );
+
+/*******************************************************************************
+ * hdl_int_list
  *
  * DESCR:       Handle list of integers
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_int_list(
+int hdl_int_list(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -41,13 +77,13 @@ int phg_handle_int_list(
    );
 
 /*******************************************************************************
- * phg_handle_asf_info
+ * hdl_asf_info
  *
  * DESCR:       Handle asf info
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_asf_info(
+int hdl_asf_info(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -55,13 +91,13 @@ int phg_handle_asf_info(
    );
 
 /*******************************************************************************
- * phg_handle_vec
+ * hdl_vec
  *
  * DESCR:       Handle vector
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_vec(
+int hdl_vec(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -69,13 +105,13 @@ int phg_handle_vec(
    );
 
 /*******************************************************************************
- * phg_handle_local_tran
+ * hdl_local_tran
  *
  * DESCR:       Handle local transformation
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_local_tran(
+int hdl_local_tran(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -83,13 +119,13 @@ int phg_handle_local_tran(
    );
 
 /*******************************************************************************
- * phg_handle_local_tran3
+ * hdl_local_tran3
  *
  * DESCR:       Handle local transformation 3D
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_local_tran3(
+int hdl_local_tran3(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -97,13 +133,13 @@ int phg_handle_local_tran3(
    );
 
 /*******************************************************************************
- * phg_handle_matrix
+ * hdl_matrix
  *
  * DESCR:       Handle matrix
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_matrix(
+int hdl_matrix(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -111,13 +147,13 @@ int phg_handle_matrix(
    );
 
 /*******************************************************************************
- * phg_handle_matrix3
+ * hdl_matrix3
  *
  * DESCR:       Handle matrix 3D
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_matrix3(
+int hdl_matrix3(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -125,13 +161,13 @@ int phg_handle_matrix3(
    );
 
 /*******************************************************************************
- * phg_handle_point_list
+ * hdl_point_list
  *
  * DESCR:       Handle point list
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_point_list(
+int hdl_point_list(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -139,13 +175,13 @@ int phg_handle_point_list(
    );
 
 /*******************************************************************************
- * phg_handle_point_list3
+ * hdl_point_list3
  *
  * DESCR:       Handle point list 3D
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_point_list3(
+int hdl_point_list3(
    Css_handle cssh,        
    El_handle elmt,
    caddr_t argdata,
@@ -153,13 +189,13 @@ int phg_handle_point_list3(
    );
 
 /*******************************************************************************
- * phg_handle_point_list_list
+ * hdl_point_list_list
  *
  * DESCR:       Handle list of point lists
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_point_list_list(
+int hdl_point_list_list(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -167,13 +203,13 @@ int phg_handle_point_list_list(
    );
 
 /*******************************************************************************
- * phg_handle_point_list_list3
+ * hdl_point_list_list3
  *
  * DESCR:       Handle list of point lists 3D
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_point_list_list3(
+int hdl_point_list_list3(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -181,13 +217,13 @@ int phg_handle_point_list_list3(
    );
 
 /*******************************************************************************
- * phg_handle_fasd3
+ * hdl_fasd3
  *
  * DESCR:       Handle facet list 3D
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_fasd3(
+int hdl_fasd3(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -195,13 +231,13 @@ int phg_handle_fasd3(
    );
 
 /*******************************************************************************
- * phg_handle_sofas3
+ * hdl_sofas3
  *
  * DESCR:       Handle set of facet lists 3D
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_sofas3(
+int hdl_sofas3(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -209,13 +245,13 @@ int phg_handle_sofas3(
    );
 
 /*******************************************************************************
- * phg_handle_text_align
+ * hdl_text_align
  *
  * DESCR:       Handle text alignment
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_text_align(
+int hdl_text_align(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -223,13 +259,13 @@ int phg_handle_text_align(
    );
 
 /*******************************************************************************
- * phg_handle_text
+ * hdl_text
  *
  * DESCR:       Handle text
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_text(
+int hdl_text(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -237,13 +273,13 @@ int phg_handle_text(
    );
 
 /*******************************************************************************
- * phg_handle_int
+ * hdl_int
  *
  * DESCR:       Handle integer data
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_int(
+int hdl_int(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -251,13 +287,13 @@ int phg_handle_int(
    );
 
 /*******************************************************************************
- * phg_handle_float
+ * hdl_float
  *
  * DESCR:       Handle float data
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_float(
+int hdl_float(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -265,13 +301,13 @@ int phg_handle_float(
    );
 
 /*******************************************************************************
- * phg_handle_gcolr
+ * hdl_gcolr
  *
  * DESCR:       Handle colour
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_gcolr(
+int hdl_gcolr(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -279,13 +315,13 @@ int phg_handle_gcolr(
    );
 
 /*******************************************************************************
- * phg_handle_lss
+ * hdl_lss
  *
  * DESCR:       Handle light source state
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_lss(
+int hdl_lss(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
@@ -293,13 +329,13 @@ int phg_handle_lss(
    );
 
 /*******************************************************************************
- * phg_handle_refl_props
+ * hdl_refl_props
  *
  * DESCR:       Handler surface reflectance properties
  * RETURNS:     TRUE on success, otherwise FALSE
  */
 
-int phg_handle_refl_props(
+int hdl_refl_props(
    Css_handle cssh,
    El_handle elmt,
    caddr_t argdata,
