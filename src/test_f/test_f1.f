1397d0f247bf39e18bba2edc6b23e8d5475d30f2
alexuspol <alexuspol@e18fabcd-5fc6-4edd-8a17-43b1c1a820ff>
2013-08-23 08:35:08 +0000

    Changed all command to lowercase

--- src/test_f/test_f1.f
+++ src/test_f/test_f1.f
@@ -1,40 +1,40 @@
-	PROGRAM DRAWLINE
+	program drawline
 
-C	Line coordinates
-	REAL PXA(2)
-	REAL PYA(2)
+c	Line coordinates
+	real pxa(2)
+	real pya(2)
 
-C	Open PHIGS and a workstation
-	CALL POPPH(0, 0)
-	CALL POPWK(0, 0, 0)
+c	Open PHIGS and a workstation
+	call popph(0, 0)
+	call popwk(0, 0, 0)
 
-C	Open structure
-	CALL POPST(0)
+c	Open structure
+	call popst(0)
 
-C	Draw line from lower left to top right
-	PXA(1) = 0.0
-	PYA(1) = 0.0
-	PXA(2) = 1.0
-	PYA(2) = 1.0
-	CALL PPL(2, pxa, pya)
+c	Draw line from lower left to top right
+	pxa(1) = 0.0
+	pya(1) = 0.0
+	pxa(2) = 1.0
+	pya(2) = 1.0
+	call ppl(2, pxa, pya)
 
-C	Draw line from lower right to top left
-	PXA(1) = 1.0
-	PYA(1) = 0.0
-	PXA(2) = 0.0
-	PYA(2) = 1.0
-	CALL PPL(2, pxa, pya)
+c	Draw line from lower right to top left
+	pxa(1) = 1.0
+	pya(1) = 0.0
+	pxa(2) = 0.0
+	pya(2) = 1.0
+	call ppl(2, pxa, pya)
 
-C	Close structure
-	CALL PCLST
+c	Close structure
+	call pclst
 
-C	Post structure to workstation
-	CALL PPOST(0, 0, 0.0)
+c	Post structure to workstation
+	call ppost(0, 0, 0.0)
 
-C	Buisy loop
-	DO WHILE (1 .GT. 0)
-	END DO
+c	Buisy loop
+	do while (1 .gt. 0)
+	end do
 
-	STOP
-	END
+	stop
+	end
 
