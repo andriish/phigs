2c702bf021063bcc2795e02232efc6b855512c2a
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-19 20:14:55 +0100

    Added CI

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 set -x
 brew install wget coreutils gcc cmake
+brew update
+brew cleanup
+brew cask cleanup
 brew cask install xquartz
 
 cmake -S. -BBUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/INSTALL -DCMAKE_Fortran_COMPILER=gfortran-11
