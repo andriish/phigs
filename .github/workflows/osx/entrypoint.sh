f0df20fe590277f79af7aab63e7ed48dbaa7bf01
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-19 19:36:48 +0100

    Added CI

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -3,6 +3,6 @@ set -x
 brew install wget coreutils gcc cmake
 brew cask install xquartz
 
-cmake -S. -BBUILD -CMAKE_INSTALL_PREFIX=$(pwd)/INSTALL
+cmake -S. -BBUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/INSTALL
 cmake --build BUILD 
 cmake --install BUILD 
