bd8f0e247d5fa86e5378a26bbf7f5142b7bf0e14
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-19 20:01:16 +0100

    Added CI

--- .github/workflows/centos7/entrypoint.sh
+++ .github/workflows/centos7/entrypoint.sh
@@ -4,11 +4,12 @@ uname -a
 cat /etc/issue
 yum -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem 
 yum -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor  libSM-devel libSM libICE libICE-devel libXext-devel libXext
-yum -y install  xorg-x11-server-Xorg mesa-libGL-devel
+yum -y install  xorg-x11-server-Xorg mesa-libGL-devel motif-devel
+yum -y install  cmake3*
 
-cmake -S. -BBUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/INSTALL
-cmake --build BUILD 
-cmake --install BUILD 
+cmake3 -S. -BBUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/INSTALL
+cmake3 --build BUILD 
+cmake3 --install BUILD 
 
 out=$?
 echo ::set-output name=out::$out
