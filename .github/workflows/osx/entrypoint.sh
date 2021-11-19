9b1228cb8dc044c8d7eddc4698c065f0f68c5eba
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-19 21:05:29 +0100

    Motif

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 set -x
 brew install wget coreutils gcc openmotif
-#brew  install --cask  xquartz
+brew  install --cask  xquartz
 
 cmake -S. -BBUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/INSTALL -DCMAKE_Fortran_COMPILER=gfortran-11
 cmake --build BUILD 
